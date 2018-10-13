723a4b882df5ffbb71034ad502aa580de7824d98
cobunny <rrpixel@gmail.com>
2018-10-12 20:54:35 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+const app = express();
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello!!!'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Running on port ${port}`));
