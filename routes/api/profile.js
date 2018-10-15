e48413aa8a8e0326036e5f245c1292a6d15ef31a
cobunny <rrpixel@gmail.com>
2018-10-14 22:08:08 -0700

    Add delete user and profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -260,4 +260,17 @@ router.delete(
   }
 );
 
+// Delete User and profile route
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() =>
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      )
+    );
+  }
+);
+
 module.exports = router;
