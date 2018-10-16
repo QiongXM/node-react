ced20efb185209e8a8ed91b1580174364e005437
cobunny <rrpixel@gmail.com>
2018-10-15 20:21:05 -0700

    Add comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -117,6 +117,39 @@ router.post(
   }
 );
 
+// Add comment route
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        if (!post) {
+          res.status(404).json({ nopost: 'Post is not found!' });
+        }
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ nopost: 'Post is not found!' }));
+  }
+);
+
 //Delete post route
 router.delete(
   '/:id',
