7e4ff61e7bcdd85aa546208d53a270d4c7b16d67
cobunny <rrpixel@gmail.com>
2018-10-18 23:51:12 -0700

    Check if user logged in and if so, keep user authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import setCurrentUser from './actions/setCurrentUser';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,6 +15,16 @@ import Login from './components/auth/Login';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+  //Set token to auth header
+  setAuthToken(localStorage.jwtToken);
+  //Decode token to get user info and expiration
+  const decoded = jwtDecode(localStorage.jwtToken);
+  //Set current user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
